06f8b56245d15d4da4eba9bffd87b155f2c76c4e
jfranco <jorge.franco.leza@gmail.com>
2014-07-28 12:46:32 +0200

    Initial @baseScript support

--- src/test/groovy/org/grooscript/TestBaseScript.groovy
+++ src/test/groovy/org/grooscript/TestBaseScript.groovy
@@ -1,16 +1,19 @@
 package org.grooscript
 
 import org.grooscript.test.ConversionMixin
+import org.grooscript.util.Util
+import spock.lang.IgnoreIf
 import spock.lang.Specification
 
 /**
  * Created by jorge on 27/07/14.
  */
 @Mixin([ConversionMixin])
+@IgnoreIf({ !Util.groovyVersionAtLeast('2.2') })
 class TestBaseScript extends Specification {
 
     void 'initial base script'() {
         expect:
-        convertAndEvaluate('baseScript/initial', true, [consoleInfo: true])
+        convertAndEvaluate('baseScript/initial')
     }
 }
