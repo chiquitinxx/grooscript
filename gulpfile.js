73cc23543188d89d3610e46646499972d42f9252
Jorge Franco <jorge.franco.leza@gmail.com>
2014-08-19 19:16:01 +0200

    Mocha tests with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
 
 gulp.task('compress', function() {
     return gulp.src('src/main/resources/META-INF/resources/grooscript.js')
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest('src/main/resources/META-INF/resources'))
+});
+
+gulp.task('tests', function () {
+    return gulp.src('src/test/js/test.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}));
 });
 
 gulp.task('default', function() {
