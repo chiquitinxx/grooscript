52f7628aaac4ced23fa042b9e454d1a0712fc5bd
Jorge Franco <jorge.franco.leza@gmail.com>
2014-10-17 23:22:20 +0200

    Added trait test

--- src/test/resources/contribution/Dinko.groovy
+++ src/test/resources/contribution/Dinko.groovy
@@ -10,3 +10,14 @@ def a = 5
 assert a == 5
 assert 3 == ( a = 3 )
 assert a == 3
+
+trait TraitA {
+    def foo
+    def getBar() { foo = 42 }
+}
+
+class ClassC implements TraitA {}
+
+def classc = new ClassC()
+assert classc.bar == 42 // In groovy returns null, a bug in groovy 2.3.7
+assert classc.foo == 42
